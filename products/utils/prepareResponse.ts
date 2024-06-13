8a14f151f3f3f8ee5c1120e0ce0aede60f379994
CarphatianSnake <sunsn4ke@gmail.com>
2024-06-13 12:18:10 +0300

    fix: add app/json content type to response

--- products/utils/prepareResponse.ts
+++ products/utils/prepareResponse.ts
@@ -9,6 +9,9 @@ type IBody = ErrorMessage | Product | Product[];
 const prepareResponse = (statusCode: number, body: IBody) => {
   return {
     statusCode,
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(body),
   };
 };
