0feccc44d9246a43b11aa06e4b8e0a174fd8c0ad
CarphatianSnake <sunsn4ke@gmail.com>
2024-06-14 14:31:52 +0300

    refactor: add 404 behaviour to getProduct handler

--- products/getProducts/index.ts
+++ products/getProducts/index.ts
@@ -2,6 +2,14 @@ import { products } from './products';
 import prepareResponse from './prepareResponse';
 import type { HttpResponse } from '@/types';
 
+export const getProductsData = () => products;
+
 export const handler = async (): HttpResponse => {
-  return prepareResponse(200, products);
+  const data = getProductsData();
+
+  if (data.length) {
+    return prepareResponse(200, data);
+  };
+
+  return prepareResponse(404, { message: 'No products found' });
 };
--- test/products/getProducts.test.ts
+++ test/products/getProducts.test.ts
@@ -1,9 +1,19 @@
-import { handler } from "@/products/getProducts";
+import * as getProducts from "@/products/getProducts";
 import { products } from "../mock/products";
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe('getProducts handler', () => {
-  it("Should return equal products array", async () => {
-    const response = await handler();
+  it("Should return products array", async () => {
+    const response = await getProducts.handler();
     expect(JSON.parse(response.body)).toStrictEqual(products);
+  });
+
+  it("Should return statusCode 404", async () => {
+    jest.spyOn(getProducts, 'getProductsData').mockReturnValue([]);
+    const response = await getProducts.handler();
+    expect(response.statusCode).toEqual(404);
   });
 })
