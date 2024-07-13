52ce5633e35e0bd290d37aa59c3ff048c8286cd9
CarphatianSnake <sunsn4ke@gmail.com>
2024-07-13 15:00:46 +0300

    refactor: remove useless layer for basicAuthorization lambda

--- services/authorization/lib/authorization-service-stack.ts
+++ services/authorization/lib/authorization-service-stack.ts
@@ -2,24 +2,17 @@ import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-import { LAYERS_PATH, SERVICES_PATH } from '@/constants';
+import { SERVICES_PATH } from '@/constants';
 import path = require('node:path');
 
 export class AuthorizationServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create lambda layers
-    const utilsLayer = new lambda.LayerVersion(this, 'UtilsLayer', {
-      code: lambda.Code.fromAsset(path.join(LAYERS_PATH, 'utils')),
-      compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
-    });
-
     // Create basic authorization service lambda
     const basicAuthorizer = new NodejsFunction(this, 'BasicAuthHandler', {
       runtime: lambda.Runtime.NODEJS_20_X,
       entry: path.join(SERVICES_PATH.Authorization, 'lambdas', 'basicAuthorizer.ts'),
-      layers: [utilsLayer],
       environment: {
         CarphatianSnake: process.env.CarphatianSnake!
       }
