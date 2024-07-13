c26ca07230d1daec396b10b0674d1c29e6a6e2bf
CarphatianSnake <sunsn4ke@gmail.com>
2024-07-13 13:03:41 +0300

    refactor: refactor basicAuthorizer lambda

--- services/authorization/lambdas/basicAuthorizer.ts
+++ services/authorization/lambdas/basicAuthorizer.ts
@@ -1,31 +1,25 @@
 import type { APIGatewayAuthorizerResult, APIGatewayRequestAuthorizerEvent, Handler } from "aws-lambda";
 
-interface IEvent extends APIGatewayRequestAuthorizerEvent {
-  authorizationToken: string;
-}
-
-export const handler: Handler<IEvent> = async (event, _, cb) => {
+export const handler: Handler<APIGatewayRequestAuthorizerEvent> = async (event, _, cb) => {
   console.log('Event: ', event);
 
-  const { type, authorizationToken, methodArn } = event;
-
-  if (type !== 'REQUEST') {
-    console.log('Token type not provided.');
-    cb('Unauthorized');
-  }
-
-  if (authorizationToken === 'null') {
-    console.log('Token not provided.');
-    cb('Unauthorized');
-  }
+  const { type, headers, methodArn } = event;
 
   try {
-    const credentials = authorizationToken.replace('Basic ', '');
+    if (type !== 'REQUEST') {
+      throw new Error('Token type is not provided');
+    }
+
+    if (!headers?.authorization) {
+      throw new Error('Token is not provided');
+    }
+
+    const credentials = headers.authorization.replace('Basic ', '');
     const [username, password] = Buffer.from(credentials, 'base64').toString('utf8').split(':');
 
     console.log(`Username: ${username}, Password: ${password}`);
 
-    const effect = !process.env[username] || process.env[username] !== password ? 'Deny' : 'Allow';
+    const effect = process.env.hasOwnProperty(username) && process.env[username] === password ? 'Allow' : 'Deny';
 
     console.log(`Policy effect: ${effect}`);
 
